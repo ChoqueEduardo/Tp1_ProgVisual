421b6a74afc3b8a3ceba2ca5e1463198e27ef863
Eduardo302010 <Eduardo302010@DESKTOP-86KOOF0>
2024-04-04 15:53:03 -0300

    Se agrega paquete clase para el ejercicio 1

---
+++ TP01_ChoqueEduardo/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,18 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero1=9;
+		int numero2=6;
+		int numero3=5;
+		int numero4=33;
+		int numero5=11;
+		
+		int promedio=(numero1 + numero2 + numero3 + numero4 + numero5)/5;
+		System.out.println("El Promedio es "+ promedio);	
+
+	}
+
+}
