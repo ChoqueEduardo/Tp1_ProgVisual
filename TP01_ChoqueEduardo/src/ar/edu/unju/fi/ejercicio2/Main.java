f1d8bd9db652d7d07611ba2aee3a9bafe4310898
Eduardo302010 <Eduardo302010@DESKTOP-86KOOF0>
2024-04-15 22:46:56 -0300

    se agrega para ejercicio 2

---
+++ TP01_ChoqueEduardo/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombrepais = "Brasil";
+		int edadpersona = 38;
+		double alturaedificio = 250.0; //metros 
+		double precioproducto = 150.50; //pesos
+		String numerotelefono = "+553585256974";
+		double calcoseno = Math.cos(0.5);
+		
+		//Mostrar por consola
+		System.out.println("Nombre de un Pais: " + nombrepais);
+		System.out.println("Edad de una persona: " + edadpersona);
+		System.out.println("Altura de un edificio: " + alturaedificio);
+		System.out.println("Precio del producto: " + precioproducto);
+		System.out.println("Nº de telefono: " + numerotelefono);
+		System.out.println("Calculo del coseno de 0.5: " + calcoseno);
+		
+
+	}
+
+}
